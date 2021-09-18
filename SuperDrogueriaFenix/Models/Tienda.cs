d64dfe3533372af9556dca3ab37e1e32ad27b0b0
omar <oaramirezr@ut.edu.co>
2021-09-18 14:20:48 -0500

    modelo clientes, tiendas, peoductos

---
+++ SuperDrogueriaFenix/Models/Tienda.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperDrogueriaFenix.Models
+{
+    public class Tienda
+    {
+
+        [Key]
+        public int IdTienda { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string RUC { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Dirección")]
+        public string Direccion { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Telefono { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Activo { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha Registro")]
+        public DateTime FechaRegistro { get; set; }
+    }
+}
