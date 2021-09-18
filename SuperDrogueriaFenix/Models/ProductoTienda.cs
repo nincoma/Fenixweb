d0b6e9dd82af4dae72b222fcb9328353cd29ed08
omar <oaramirezr@ut.edu.co>
2021-09-18 16:02:15 -0500

    models omr agregados

---
+++ SuperDrogueriaFenix/Models/ProductoTienda.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperDrogueriaFenix.Models
+{
+    public class ProductoTienda
+    {
+
+        [Key]
+        public int IdProductoTienda { get; set; }
+
+
+        [Required]
+        public decimal PrecioUnidadCompra { get; set; }
+
+        [Required]
+        public decimal PrecioUnidadVenta { get; set; }
+
+        [Required]
+
+        public int Stock { get; set; }
+
+        [Required]
+        public bool Activo { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha Registro")]
+        public DateTime FechaRegistro { get; set; }
+
+        public Cliente Cliente { get; set; }
+
+        public Tienda Tienda { get; set; }
+    }
+}
+
---
+++ SuperDrogueriaFenix/Models/Tienda.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperDrogueriaFenix.Models
+{
+    public class Tienda
+    {
+
+        [Key]
+        public int IdTienda { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string RUC { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Dirección")]
+        public string Direccion { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Telefono { get; set; }
+
+        [Required]
+        public bool Activo { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha Registro")]
+        public DateTime FechaRegistro { get; set; }
+    }
+}
